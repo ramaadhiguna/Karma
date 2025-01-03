bfe2b23a7bb102761a79b3c2e03988696db57cd2
Rama Adhiguna <ramaadhiguna@gmail.com>
2025-01-03 18:43:41 +0700

    establish graphic pipeline (cert & frag)

--- lve_test_app.hpp
+++ lve_test_app.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
 
 namespace lve {
     class FirstApp {
@@ -11,5 +12,6 @@ namespace lve {
             void run();
         private:
             LveWindow lveWindow{WIDTH, HEIGHT, "Testing Vulkan API"};
+            LvePipeline lvePipeline{"shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv"};
     };
 }
